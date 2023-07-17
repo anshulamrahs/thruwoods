7c2f6fc10ff29a71e2d954591b79e34920446e00
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-07-17 15:27:45 +0530

    components

---
+++ madanlal/src/components/download/Download.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './resume.scss'
+
+const 'Resume' = () => {
+  return (
+    <div>'Download'</div>
+  )
+}
+
+export default 'Resume'
---
+++ madanlal/src/components/papers/Papers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './papers.scss'
+
+const Papers = () => {
+  return (
+    <div>Papers</div>
+  )
+}
+
+export default Papers
