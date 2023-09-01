3ce3ecf7ca7cf16781351e7bce7f086f71a018da
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-09-02 03:45:41 +0530

    components

--- thruwoods/src/components/navbar/Navbar.jsx
+++ thruwoods/src/components/navbar/Navbar.jsx
@@ -1,27 +1,9 @@
 import React from 'react'
 
-const 
-import React from 'react'
-
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <div>Navbar</div>
   )
 }
- = () => {
-  return (
-    <div>
-      import React from 'react'
-      
-      export const Navbar = () => {
-        return (
-          <div>Navbar</div>
-        )
-      }
-      
-    </div>
-  )
-}
 
-export default 
-
+export default Navbar
